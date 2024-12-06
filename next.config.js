15aeece76e475baea7678a22cf6c799290944ea5
jiharal <jihar.akakom14@gmail.com>
2024-12-06 13:26:27 +0700

    Fix GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/ci-fest',
     images: {
       unoptimized: true
-    }
+    },
+    assetPrefix: '/ci-fest'
   }
   
   module.exports = nextConfig
